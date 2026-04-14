2eec41441e75fac1fcb6439c82d8674b3151a7f4
mc <162787712+mc-str@users.noreply.github.com>
2026-04-14 20:22:19 +0800

    Add files via upload

---
+++ music-data.js
@@ -0,0 +1,12 @@
+window.musicData = [
+    { id: 1, title: "千本樱", author: "初音未来 / 改编", size: "1.8 MB", downloads: "2.3k", downloadUrl: "music/qianbenying.zip" },
+    { id: 2, title: "Sweden", author: "C418", size: "982 KB", downloads: "4.1k", downloadUrl: "music/sweden.zip" },
+    { id: 3, title: "极乐净土", author: "GARNiDELiA", size: "2.1 MB", downloads: "1.7k", downloadUrl: "music/jilejingtu.zip" },
+    { id: 4, title: "Minecraft - 主旋律", author: "C418", size: "1.2 MB", downloads: "3.5k", downloadUrl: "music/minecraft_theme.zip" },
+    { id: 5, title: "红石狂想曲", author: "红石巨佬_小T", size: "2.4 MB", downloads: "892", downloadUrl: "music/hongshikuangxiang.zip" },
+    { id: 6, title: "卡农·MC版", author: "Pachelbel remix", size: "1.5 MB", downloads: "1.2k", downloadUrl: "music/canon_mc.zip" },
+    { id: 7, title: "Bad Apple!!", author: "东方Project", size: "2.9 MB", downloads: "3.0k", downloadUrl: "music/bad_apple.zip" },
+    { id: 8, title: "达拉崩吧", author: "ilem / 洛天依", size: "2.2 MB", downloads: "1.4k", downloadUrl: "music/dalabengba.zip" },
+    { id: 9, title: "Luv Letter", author: "Dj Okawari", size: "1.9 MB", downloads: "985", downloadUrl: "music/luv_letter.zip" },
+    { id: 10, title: "Terraria Overworld", author: "Re-Logic", size: "1.3 MB", downloads: "670", downloadUrl: "music/terraria_overworld.zip" }
+];
