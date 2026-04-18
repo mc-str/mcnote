7391385988d7006e1d4f3bdf442aa67680f9ffa1
mc <162787712+mc-str@users.noreply.github.com>
2026-04-18 08:00:21 +0800

    Add files via upload

---
+++ music-data.js
@@ -0,0 +1,66 @@
+window.musicData = [
+    {
+        id: 1,
+        title: "数据测试",
+        author: "数据测试",
+        size: "0 KB",
+        date: "9999-99-99",
+        downloadUrl: "music/id1.zip"
+    },
+    {
+        id: 2,
+        title: "数据测试",
+        author: "数据测试",
+        size: "0 KB",
+        date: "9999-99-99",
+        downloadUrl: "music/id2.zip"
+    },
+    {
+        id: 3,
+        title: "His Theme (Undertale)",
+        author: "PixelHarmony",
+        size: "210 KB",
+        date: "2025-03-01",
+        downloadUrl: "music/id3.zip"
+    },
+    {
+        id: 4,
+        title: "Minecraft - Sweden",
+        author: "C418 复刻",
+        size: "180 KB",
+        date: "2024-12-20",
+        downloadUrl: "music/id4.zip"
+    },
+    {
+        id: 5,
+        title: "Bad Apple!! 音色还原",
+        author: "指令师Leo",
+        size: "340 KB",
+        date: "2025-03-15",
+        downloadUrl: "music/id5.zip"
+    },
+    {
+        id: 6,
+        title: "千本樱 (高音优化)",
+        author: "樱之音符",
+        size: "512 KB",
+        date: "2025-03-18",
+        downloadUrl: "music/id6.zip"
+    },
+    {
+        id: 7,
+        title: "致爱丽丝 (贝多芬)",
+        author: "古典红石",
+        size: "156 KB",
+        date: "2025-02-28",
+        downloadUrl: "music/id7.zip"
+    },
+    {
+        id: 8,
+        title: "See You Again",
+        author: "Wiz Khalifa 红石",
+        size: "278 KB",
+        date: "2025-03-20",
+        downloadUrl: "music/id8.zip"
+    }
+];
