9262ef5a19d6be5d4c79d51b8421a6c2aa921cdf
mc <162787712+mc-str@users.noreply.github.com>
2026-04-18 11:16:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.appConfig = {
     // 站点运行起始时间（格式：YYYY-MM-DD 或 YYYY-MM-DDTHH:mm:ss）
-    startDate: "2026-04-18 12:00:00'",
+    startDate: "2026-04-18 12:00:00",
     // 版权文字（支持 HTML）
     copyright: "© 2026 矩阵方块",
     // 站点名称（用于页脚显示）
