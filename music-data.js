a9ae6d495bf122af3224da5ad8483affa2cdb1fe
mc <162787712+mc-str@users.noreply.github.com>
2026-04-18 17:14:57 +0800

    Add files via upload

--- music-data.js
+++ music-data.js
@@ -1,90 +1,146 @@
 window.musicData = [
-    {
-        id: 1,
-        title: "China_X",
-        author: "徐梦圆",
-        size: "4.25 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/China_X.zip"
-    },
-    {
-        id: 2,
-        title: "把回忆拼好给你",
-        author: "王贰浪",
-        size: "6.98 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/把回忆拼好给你.zip"
-    },
-    {
-        id: 3,
-        title: "演员",
-        author: "薛之谦",
-        size: "3.07 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/演员.zip"
-    },
-    {
-        id: 4,
-        title: "诀别书",
-        author: "邓垚",
-        size: "3.99 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/诀别书.zip"
-    },
-    {
-        id: 5,
-        title: "反乌托邦",
-        author: "星尘、诗岸",
-        size: "5.24 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/反乌托邦.zip"
-    },
-    {
-        id: 6,
-        title: "调查中",
-        author: "糯米Nomi",
-        size: "6.18 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/调查中.zip"
-    },
-    {
-        id: 7,
-        title: "第57次取消发送",
-        author: "陆绮菲",
-        size: "4.79 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/第57次取消发送.zip"
-    },
-    {
-        id: 8,
-        title: "此去半生",
-        author: "桃籽",
-        size: "2.31 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/此去半生.zip"
-    },
-    {
-        id: 9,
-        title: "你从未离去",
-        author: "白挺",
-        size: "7.56 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/你从未离去.zip"
-    },
-    {
-        id: 10,
-        title: "兰亭序",
-        author: "周杰伦",
-        size: "3.91 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/兰亭序.zip"
-    },
-    {
-        id: 11,
-        title: "辞九门回忆",
-        author: "冰幽、解忧草",
-        size: "1.77 KB",
-        date: "2026-04-18",
-        downloadUrl: "music/辞九门回忆.zip"
-    }
+{
+id: 1,
+title: "义勇军进行曲",
+author: "聂耳/田汉/中国交响乐、军乐团、国音合唱团",
+size: "4.47 KB",
+date: "2026-04-18",
+downloadUrl: "music/义勇军进行曲.zip"
+},
+{
+id: 2,
+title: "China_X",
+author: "徐梦圆",
+size: "4.25 KB",
+date: "2026-04-18",
+downloadUrl: "music/China_X.zip"
+},
+{
+id: 3,
+title: "把回忆拼好给你",
+author: "王贰浪",
+size: "6.98 KB",
+date: "2026-04-18",
+downloadUrl: "music/把回忆拼好给你.zip"
+},
+{
+id: 4,
+title: "演员",
+author: "薛之谦",
+size: "3.07 KB",
+date: "2026-04-18",
+downloadUrl: "music/演员.zip"
+},
+{
+id: 5,
+title: "诀别书",
+author: "邓垚",
+size: "3.99 KB",
+date: "2026-04-18",
+downloadUrl: "music/诀别书.zip"
+},
+{
+id: 6,
+title: "反乌托邦",
+author: "星尘、诗岸",
+size: "5.24 KB",
+date: "2026-04-18",
+downloadUrl: "music/反乌托邦.zip"
+},
+{
+id: 7,
+title: "调查中",
+author: "糯米Nomi",
+size: "6.18 KB",
+date: "2026-04-18",
+downloadUrl: "music/调查中.zip"
+},
+{
+id: 8,
+title: "第57次取消发送",
+author: "陆绮菲",
+size: "4.79 KB",
+date: "2026-04-18",
+downloadUrl: "music/第57次取消发送.zip"
+},
+{
+id: 9,
+title: "此去半生",
+author: "桃籽",
+size: "2.31 KB",
+date: "2026-04-18",
+downloadUrl: "music/此去半生.zip"
+},
+{
+id: 10,
+title: "你从未离去",
+author: "白挺",
+size: "7.56 KB",
+date: "2026-04-18",
+downloadUrl: "music/你从未离去.zip"
+},
+{
+id: 11,
+title: "兰亭序",
+author: "周杰伦",
+size: "3.91 KB",
+date: "2026-04-18",
+downloadUrl: "music/兰亭序.zip"
+},
+{
+id: 12,
+title: "辞九门回忆",
+author: "冰幽、解忧草",
+size: "1.77 KB",
+date: "2026-04-18",
+downloadUrl: "music/辞九门回忆.zip"
+},
+{
+id: 13,
+title: "摇篮曲",
+author: "Rauf & Faik",
+size: "23.03 KB",
+date: "2026-04-18",
+downloadUrl: "music/摇篮曲.zip"
+},
+{
+id: 14,
+title: "说了再见",
+author: "周杰伦",
+size: "15.79 KB",
+date: "2026-04-18",
+downloadUrl: "music/说了再见.zip"
+},
+{
+id: 15,
+title: "室内系的TrackMaker",
+author: "Yunomi / nicamoq",
+size: "41.38 KB",
+date: "2026-04-18",
+downloadUrl: "music/室内系的TrackMake.zip"
+},
+{
+id: 16,
+title: "千本樱",
+author: "初音未来",
+size: "99.87 KB",
+date: "2026-04-18",
+downloadUrl: "music/千本樱.zip"
+},
+{
+id: 17,
+title: "凄美地",
+author: "郭顶",
+size: "75.83 KB",
+date: "2026-04-18",
+downloadUrl: "music/凄美地.zip"
+},
+{
+id: 18,
+title: "浮光 The History",
+author: "Jannik",
+size: "32.12 KB",
+date: "2026-04-18",
+downloadUrl: "music/浮光 The_History.zip"
+}
 ];
