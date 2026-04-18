1ce2f80ce07cae0defaa9be6a0eb30d7144aaa2e
mc <162787712+mc-str@users.noreply.github.com>
2026-04-18 12:39:19 +0800

    Add files via upload

--- music-data.js
+++ music-data.js
@@ -1,90 +1,90 @@
 window.musicData = [
     {
         id: 1,
-        title: "你从未离去",
-        author: "白挺",
-        size: "32.32 K",
+        title: "China_X",
+        author: "徐梦圆",
+        size: "4.25 KB",
         date: "2026-04-18",
-        downloadUrl: "music/你从未离去.zip"
+        downloadUrl: "music/China_X.zip"
     },
     {
         id: 2,
-        title: "China_X",
-        author: "徐梦圆",
-        size: "23.83 K",
+        title: "把回忆拼好给你",
+        author: "王贰浪",
+        size: "6.98 KB",
         date: "2026-04-18",
-        downloadUrl: "music/China_X.txt"
+        downloadUrl: "music/把回忆拼好给你.zip"
     },
     {
         id: 3,
-        title: "把回忆拼好给你",
-        author: "王贰浪",
-        size: "32.81 K",
+        title: "演员",
+        author: "薛之谦",
+        size: "3.07 KB",
         date: "2026-04-18",
-        downloadUrl: "music/把回忆拼好给你.txt"
+        downloadUrl: "music/演员.zip"
     },
     {
         id: 4,
-        title: "辞九门回忆",
-        author: "冰幽、解忧草",
-        size: "1.73 K",
+        title: "诀别书",
+        author: "邓垚",
+        size: "3.99 KB",
         date: "2026-04-18",
-        downloadUrl: "music/辞九门回忆.zip"
+        downloadUrl: "music/诀别书.zip"
     },
     {
         id: 5,
-        title: "此去半生",
-        author: "桃籽",
-        size: "10.79 K",
+        title: "反乌托邦",
+        author: "星尘、诗岸",
+        size: "5.24 KB",
         date: "2026-04-18",
-        downloadUrl: "music/此去半生.txt"
+        downloadUrl: "music/反乌托邦.zip"
     },
     {
         id: 6,
-        title: "第57次取消发送",
-        author: "陆绮菲",
-        size: "23.92 K",
+        title: "调查中",
+        author: "糯米Nomi",
+        size: "6.18 KB",
         date: "2026-04-18",
-        downloadUrl: "music/第57次取消发送.txt"
+        downloadUrl: "music/调查中.zip"
     },
     {
         id: 7,
-        title: "调查中",
-        author: "糯米Nomi",
-        size: "30.93 K",
+        title: "第57次取消发送",
+        author: "陆绮菲",
+        size: "4.79 KB",
         date: "2026-04-18",
-        downloadUrl: "music/调查中.txt"
+        downloadUrl: "music/第57次取消发送.zip"
     },
     {
         id: 8,
-        title: "反乌托邦",
-        author: "星辰、诗岸",
-        size: "28.95 K",
+        title: "此去半生",
+        author: "桃籽",
+        size: "2.31 KB",
         date: "2026-04-18",
-        downloadUrl: "music/反乌托邦.txt"
+        downloadUrl: "music/此去半生.zip"
     },
     {
         id: 9,
-        title: "诀别书",
-        author: "邓壵",
-        size: "20.86 K",
-        date: "2025-02-28",
-        downloadUrl: "music/诀别书.txt"
+        title: "你从未离去",
+        author: "白挺",
+        size: "7.56 KB",
+        date: "2026-04-18",
+        downloadUrl: "music/你从未离去.zip"
     },
     {
         id: 10,
         title: "兰亭序",
         author: "周杰伦",
-        size: "3.82 K",
+        size: "3.91 KB",
         date: "2026-04-18",
         downloadUrl: "music/兰亭序.zip"
     },
     {
         id: 11,
-        title: "演员",
-        author: "薛之谦",
-        size: "15.14 K",
+        title: "辞九门回忆",
+        author: "冰幽、解忧草",
+        size: "1.77 KB",
         date: "2026-04-18",
-        downloadUrl: "music/演员.txt"
+        downloadUrl: "music/辞九门回忆.zip"
     }
 ];
