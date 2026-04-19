f1fab973e356122f06ee55d4ebde3cdb0f64255b
mc <162787712+mc-str@users.noreply.github.com>
2026-04-19 09:25:02 +0800

    Add files via upload

--- music-data.js
+++ music-data.js
@@ -9,6 +9,14 @@ downloadUrl: "music/义勇军进行曲.zip"
 },
 {
 id: 2,
+title: "歌唱祖国",
+author: "王莘/中央乐团合唱团",
+size: "7.88 KB",
+date: "2026-04-19",
+downloadUrl: "music/歌唱祖国.zip"
+},
+{
+id: 3,
 title: "China_X",
 author: "徐梦圆",
 size: "4.25 KB",
@@ -16,7 +24,7 @@ date: "2026-04-18",
 downloadUrl: "music/China_X.zip"
 },
 {
-id: 3,
+id: 4,
 title: "把回忆拼好给你",
 author: "王贰浪",
 size: "6.98 KB",
@@ -24,7 +32,7 @@ date: "2026-04-18",
 downloadUrl: "music/把回忆拼好给你.zip"
 },
 {
-id: 4,
+id: 5,
 title: "演员",
 author: "薛之谦",
 size: "3.07 KB",
@@ -32,7 +40,7 @@ date: "2026-04-18",
 downloadUrl: "music/演员.zip"
 },
 {
-id: 5,
+id: 6,
 title: "诀别书",
 author: "邓垚",
 size: "3.99 KB",
@@ -40,7 +48,7 @@ date: "2026-04-18",
 downloadUrl: "music/诀别书.zip"
 },
 {
-id: 6,
+id: 7,
 title: "反乌托邦",
 author: "星尘、诗岸",
 size: "5.24 KB",
@@ -48,7 +56,7 @@ date: "2026-04-18",
 downloadUrl: "music/反乌托邦.zip"
 },
 {
-id: 7,
+id: 8,
 title: "调查中",
 author: "糯米Nomi",
 size: "6.18 KB",
@@ -56,7 +64,7 @@ date: "2026-04-18",
 downloadUrl: "music/调查中.zip"
 },
 {
-id: 8,
+id: 9,
 title: "第57次取消发送",
 author: "陆绮菲",
 size: "4.79 KB",
@@ -64,7 +72,7 @@ date: "2026-04-18",
 downloadUrl: "music/第57次取消发送.zip"
 },
 {
-id: 9,
+id: 10,
 title: "此去半生",
 author: "桃籽",
 size: "2.31 KB",
@@ -72,7 +80,7 @@ date: "2026-04-18",
 downloadUrl: "music/此去半生.zip"
 },
 {
-id: 10,
+id: 11,
 title: "你从未离去",
 author: "白挺",
 size: "7.56 KB",
@@ -80,7 +88,7 @@ date: "2026-04-18",
 downloadUrl: "music/你从未离去.zip"
 },
 {
-id: 11,
+id: 12,
 title: "兰亭序",
 author: "周杰伦",
 size: "3.91 KB",
@@ -88,7 +96,7 @@ date: "2026-04-18",
 downloadUrl: "music/兰亭序.zip"
 },
 {
-id: 12,
+id: 13,
 title: "辞九门回忆",
 author: "冰幽、解忧草",
 size: "1.77 KB",
@@ -96,7 +104,7 @@ date: "2026-04-18",
 downloadUrl: "music/辞九门回忆.zip"
 },
 {
-id: 13,
+id: 14,
 title: "摇篮曲",
 author: "Rauf & Faik",
 size: "23.03 KB",
@@ -104,7 +112,7 @@ date: "2026-04-18",
 downloadUrl: "music/摇篮曲.zip"
 },
 {
-id: 14,
+id: 15,
 title: "说了再见",
 author: "周杰伦",
 size: "15.79 KB",
@@ -112,7 +120,7 @@ date: "2026-04-18",
 downloadUrl: "music/说了再见.zip"
 },
 {
-id: 15,
+id: 16,
 title: "室内系的TrackMaker",
 author: "Yunomi / nicamoq",
 size: "41.38 KB",
@@ -120,7 +128,7 @@ date: "2026-04-18",
 downloadUrl: "music/室内系的TrackMake.zip"
 },
 {
-id: 16,
+id: 17,
 title: "千本樱",
 author: "初音未来",
 size: "99.87 KB",
@@ -128,7 +136,7 @@ date: "2026-04-18",
 downloadUrl: "music/千本樱.zip"
 },
 {
-id: 17,
+id: 18,
 title: "凄美地",
 author: "郭顶",
 size: "75.83 KB",
@@ -136,11 +144,596 @@ date: "2026-04-18",
 downloadUrl: "music/凄美地.zip"
 },
 {
-id: 18,
+id: 19,
 title: "浮光 The History",
 author: "Jannik",
 size: "32.12 KB",
 date: "2026-04-18",
 downloadUrl: "music/浮光 The_History.zip"
+},
+// 以下是新截图里的所有音乐，按截图顺序排列
+{
+id: 20,
+title: "喜羊羊与灰太狼-左手右手",
+author: "杨沛宜",
+size: "1.58 KB",
+date: "2026-04-19",
+downloadUrl: "music/喜羊羊与灰太狼-左手右手.zip"
+},
+{
+id: 21,
+title: "太阳照常升起",
+author: "久石让",
+size: "13.14 KB",
+date: "2026-04-19",
+downloadUrl: "music/太阳照常升起.zip"
+},
+{
+id: 22,
+title: "最后一页",
+author: "江语晨",
+size: "5.45 KB",
+date: "2026-04-19",
+downloadUrl: "music/最后一页.zip"
+},
+{
+id: 23,
+title: "最后攻势",
+author: "游戏原声",
+size: "2.72 KB",
+date: "2026-04-19",
+downloadUrl: "music/最后攻势.zip"
+},
+{
+id: 24,
+title: "植物大战僵尸",
+author: "游戏原声",
+size: "2.14 KB",
+date: "2026-04-19",
+downloadUrl: "music/植物大战僵尸.zip"
+},
+{
+id: 25,
+title: "账号已注销",
+author: "太一",
+size: "2.69 KB",
+date: "2026-04-19",
+downloadUrl: "music/账号已注销.zip"
+},
+{
+id: 26,
+title: "葬花",
+author: "王媛渊",
+size: "5.34 KB",
+date: "2026-04-19",
+downloadUrl: "music/葬花.zip"
+},
+{
+id: 27,
+title: "遇见",
+author: "孙燕姿",
+size: "2.54 KB",
+date: "2026-04-19",
+downloadUrl: "music/遇见.zip"
+},
+{
+id: 28,
+title: "雨爱",
+author: "杨丞琳",
+size: "3.14 KB",
+date: "2026-04-19",
+downloadUrl: "music/雨爱.zip"
+},
+{
+id: 29,
+title: "银行不妙曲",
+author: "网络改编曲",
+size: "6.82 KB",
+date: "2026-04-19",
+downloadUrl: "music/银行不妙曲.zip"
+},
+{
+id: 30,
+title: "悬溺 - 原琴版",
+author: "葛东琪",
+size: "3.1 KB",
+date: "2026-04-19",
+downloadUrl: "music/悬溺原琴版.zip"
+},
+{
+id: 31,
+title: "星光下的梦想",
+author: "郭易",
+size: "1.04 KB",
+date: "2026-04-19",
+downloadUrl: "music/星光下的梦想.zip"
+},
+{
+id: 32,
+title: "星茶会",
+author: "纯音乐",
+size: "3.86 KB",
+date: "2026-04-19",
+downloadUrl: "music/星茶会.zip"
+},
+{
+id: 33,
+title: "心似烟火",
+author: "陈壹千",
+size: "3.82 KB",
+date: "2026-04-19",
+downloadUrl: "music/心似烟火.zip"
+},
+{
+id: 34,
+title: "小半",
+author: "陈粒",
+size: "2.48 KB",
+date: "2026-04-19",
+downloadUrl: "music/小半.zip"
+},
+{
+id: 35,
+title: "我的歌声里",
+author: "曲婉婷",
+size: "2.77 KB",
+date: "2026-04-19",
+downloadUrl: "music/我的歌声里.zip"
+},
+{
+id: 36,
+title: "唯一",
+author: "告五人",
+size: "5.06 KB",
+date: "2026-04-19",
+downloadUrl: "music/唯一.zip"
+},
+{
+id: 37,
+title: "听妈妈的话",
+author: "周杰伦",
+size: "6.51 KB",
+date: "2026-04-19",
+downloadUrl: "music/听妈妈的话.zip"
+},
+{
+id: 38,
+title: "所念皆星河",
+author: "CMJ",
+size: "3.39 KB",
+date: "2026-04-19",
+downloadUrl: "music/所念皆星河.zip"
+},
+{
+id: 39,
+title: "溯",
+author: "CORSAK胡梦周 / 马吟吟",
+size: "2.18 KB",
+date: "2026-04-19",
+downloadUrl: "music/溯.zip"
+},
+{
+id: 40,
+title: "说了再见",
+author: "周杰伦",
+size: "6.57 KB",
+date: "2026-04-19",
+downloadUrl: "music/说了再见.zip"
+},
+{
+id: 41,
+title: "圣诞快乐劳伦斯先生",
+author: "坂本龙一",
+size: "6.18 KB",
+date: "2026-04-19",
+downloadUrl: "music/圣诞快乐劳伦斯先生.zip"
+},
+{
+id: 42,
+title: "让风告诉你",
+author: "花玲 / 喵酱 / kinsen / 鹿喑kana",
+size: "3.25 KB",
+date: "2026-04-19",
+downloadUrl: "music/让风告诉你.zip"
+},
+{
+id: 43,
+title: "辞青",
+author: "网络古风",
+size: "4.28 KB",
+date: "2026-04-19",
+downloadUrl: "music/辞青.zip"
+},
+{
+id: 44,
+title: "缺氧",
+author: "杨丞琳",
+size: "3.15 KB",
+date: "2026-04-19",
+downloadUrl: "music/缺氧.zip"
+},
+{
+id: 45,
+title: "权御天下",
+author: "洛天依",
+size: "9.75 KB",
+date: "2026-04-19",
+downloadUrl: "music/权御天下.zip"
+},
+{
+id: 46,
+title: "晴天",
+author: "周杰伦",
+size: "2.58 KB",
+date: "2026-04-19",
+downloadUrl: "music/晴天.zip"
+},
+{
+id: 47,
+title: "情绪回收站",
+author: "失落花园_",
+size: "4.47 KB",
+date: "2026-04-19",
+downloadUrl: "music/情绪回收站.zip"
+},
+{
+id: 48,
+title: "青衣",
+author: "马天宇",
+size: "1.83 KB",
+date: "2026-04-19",
+downloadUrl: "music/青衣.zip"
+},
+{
+id: 49,
+title: "青花瓷",
+author: "周杰伦",
+size: "3.62 KB",
+date: "2026-04-19",
+downloadUrl: "music/青花瓷.zip"
+},
+{
+id: 50,
+title: "千恋万花",
+author: "游戏原声",
+size: "5.57 KB",
+date: "2026-04-19",
+downloadUrl: "music/千恋万花.zip"
+},
+{
+id: 51,
+title: "千本樱",
+author: "初音未来",
+size: "4.57 KB",
+date: "2026-04-19",
+downloadUrl: "music/千本樱.zip"
+},
+{
+id: 52,
+title: "起风了",
+author: "买辣椒也用券",
+size: "3.1 KB",
+date: "2026-04-19",
+downloadUrl: "music/起风了.zip"
+},
+{
+id: 53,
+title: "七里香",
+author: "周杰伦",
+size: "1.44 KB",
+date: "2026-04-19",
+downloadUrl: "music/七里香.zip"
+},
+{
+id: 54,
+title: "蒲公英的约定",
+author: "周杰伦",
+size: "4.17 KB",
+date: "2026-04-19",
+downloadUrl: "music/蒲公英的约定.zip"
+},
+{
+id: 55,
+title: "平凡之路",
+author: "朴树",
+size: "6.26 KB",
+date: "2026-04-19",
+downloadUrl: "music/平凡之路.zip"
+},
+{
+id: 56,
+title: "偏爱",
+author: "张芸京",
+size: "3.37 KB",
+date: "2026-04-19",
+downloadUrl: "music/偏爱.zip"
+},
+{
+id: 57,
+title: "你离开的事实",
+author: "高至豪",
+size: "4.1 KB",
+date: "2026-04-19",
+downloadUrl: "music/你离开的事实.zip"
+},
+{
+id: 58,
+title: "鸢之诗",
+author: "网络古风",
+size: "2.51 KB",
+date: "2026-04-19",
+downloadUrl: "music/鸢之诗.zip"
+},
+{
+id: 59,
+title: "莫愁乡",
+author: "亚细亚旷世奇才",
+size: "3.34 KB",
+date: "2026-04-19",
+downloadUrl: "music/莫愁乡.zip"
+},
+{
+id: 60,
+title: "迷你世界大厅音乐",
+author: "迷你世界游戏原声",
+size: "815 B",
+date: "2026-04-19",
+downloadUrl: "music/迷你世界大厅音乐.zip"
+},
+{
+id: 61,
+title: "梦回还",
+author: "呦猫UNEKO",
+size: "3.1 KB",
+date: "2026-04-19",
+downloadUrl: "music/梦回还.zip"
+},
+{
+id: 62,
+title: "罗生门",
+author: "麦浚龙 / 谢安琪",
+size: "2.28 KB",
+date: "2026-04-19",
+downloadUrl: "music/罗生门.zip"
+},
+{
+id: 63,
+title: "柳轻颂 - 向日葵",
+author: "柳轻颂",
+size: "2.84 KB",
+date: "2026-04-19",
+downloadUrl: "music/柳轻颂向日葵.zip"
+},
+{
+id: 64,
+title: "离开我的依赖",
+author: "王靖雯",
+size: "3.44 KB",
+date: "2026-04-19",
+downloadUrl: "music/离开我的依赖.zip"
+},
+{
+id: 65,
+title: "懒羊羊当大厨",
+author: "李紫昕",
+size: "2.1 KB",
+date: "2026-04-19",
+downloadUrl: "music/懒羊羊当大厨.zip"
+},
+{
+id: 66,
+title: "兰亭序",
+author: "周杰伦",
+size: "3.6 KB",
+date: "2026-04-19",
+downloadUrl: "music/兰亭序.zip"
+},
+{
+id: 67,
+title: "克罗地亚狂想曲",
+author: "马克西姆",
+size: "4.74 KB",
+date: "2026-04-19",
+downloadUrl: "music/克罗地亚狂想曲.zip"
+},
+{
+id: 68,
+title: "可能",
+author: "程响",
+size: "2.64 KB",
+date: "2026-04-19",
+downloadUrl: "music/可能.zip"
+},
+{
+id: 69,
+title: "开往春天的地铁",
+author: "羽泉",
+size: "1.88 KB",
+date: "2026-04-19",
+downloadUrl: "music/开往春天的地铁.zip"
+},
+{
+id: 70,
+title: "菊次郎的夏天",
+author: "久石让",
+size: "6.68 KB",
+date: "2026-04-19",
+downloadUrl: "music/菊次郎的夏天.zip"
+},
+{
+id: 71,
+title: "精卫",
+author: "陈粒",
+size: "4.07 KB",
+date: "2026-04-19",
+downloadUrl: "music/精卫.zip"
+},
+{
+id: 72,
+title: "江上清风游",
+author: "变奏的梦想",
+size: "4.67 KB",
+date: "2026-04-19",
+downloadUrl: "music/江上清风游.zip"
+},
+{
+id: 73,
+title: "江南",
+author: "林俊杰",
+size: "3.86 KB",
+date: "2026-04-19",
+downloadUrl: "music/江南.zip"
+},
+{
+id: 74,
+title: "回来吧，刺激战场",
+author: "郑佳磊",
+size: "759 B",
+date: "2026-04-19",
+downloadUrl: "music/回来吧刺激战场.zip"
+},
+{
+id: 75,
+title: "黄金之风",
+author: "动画原声",
+size: "2.17 KB",
+date: "2026-04-19",
+downloadUrl: "music/黄金之风.zip"
+},
+{
+id: 76,
+title: "幻想是痛的延续",
+author: "网络二创",
+size: "1.65 KB",
+date: "2026-04-19",
+downloadUrl: "music/幻想是痛的延续.zip"
+},
+{
+id: 77,
+title: "欢乐斗地主",
+author: "游戏原声",
+size: "674 B",
+date: "2026-04-19",
+downloadUrl: "music/欢乐斗地主.zip"
+},
+{
+id: 78,
+title: "花之舞",
+author: "DJ Okawari",
+size: "7.91 KB",
+date: "2026-04-19",
+downloadUrl: "music/花之舞.zip"
+},
+{
+id: 79,
+title: "告白",
+author: "王欣宇",
+size: "2.47 KB",
+date: "2026-04-19",
+downloadUrl: "music/告白.zip"
+},
+{
+id: 80,
+title: "花海",
+author: "周杰伦",
+size: "4.61 KB",
+date: "2026-04-19",
+downloadUrl: "music/花海.zip"
+},
+{
+id: 81,
+title: "红色高跟鞋",
+author: "蔡健雅",
+size: "2.45 KB",
+date: "2026-04-19",
+downloadUrl: "music/红色高跟鞋.zip"
+},
+{
+id: 82,
+title: "海阔天空",
+author: "Beyond",
+size: "4.71 KB",
+date: "2026-04-19",
+downloadUrl: "music/海阔天空.zip"
+},
+{
+id: 83,
+title: "光年之外",
+author: "邓紫棋",
+size: "2.31 KB",
+date: "2026-04-19",
+downloadUrl: "music/光年之外.zip"
+},
+{
+id: 84,
+title: "孤勇者",
+author: "陈奕迅",
+size: "4.43 KB",
+date: "2026-04-19",
+downloadUrl: "music/孤勇者.zip"
+},
+{
+id: 85,
+title: "恭喜发财",
+author: "刘德华",
+size: "6.99 KB",
+date: "2026-04-19",
+downloadUrl: "music/恭喜发财.zip"
+},
+{
+id: 86,
+title: "风之谷",
+author: "久石让 / 安田成美",
+size: "3 KB",
+date: "2026-04-19",
+downloadUrl: "music/风之谷.zip"
+},
+{
+id: 87,
+title: "反方向的钟",
+author: "周杰伦",
+size: "3.53 KB",
+date: "2026-04-19",
+downloadUrl: "music/反方向的钟.zip"
+},
+{
+id: 88,
+title: "哆啦A梦",
+author: "大杉久美子",
+size: "2.27 KB",
+date: "2026-04-19",
+downloadUrl: "music/哆啦A梦.zip"
+},
+{
+id: 89,
+title: "大东北",
+author: "何玉",
+size: "582 B",
+date: "2026-04-19",
+downloadUrl: "music/大东北.zip"
+},
+{
+id: 90,
+title: "打上花火",
+author: "DAOKO/米津玄师",
+size: "11.69 KB",
+date: "2026-04-19",
+downloadUrl: "music/打上花火.zip"
+},
+{
+id: 91,
+title: "催眠术",
+author: "张艺兴",
+size: "11.82 KB",
+date: "2026-04-19",
+downloadUrl: "music/催眠术.zip"
+},
+{
+id: 92,
+title: "凑热闹",
+author: "BY2",
+size: "7.25 KB",
+date: "2026-04-19",
+downloadUrl: "music/凑热闹.zip"
 }
 ];
