8d9ab6d87c00b6faefaa472a4fcf846899a7672d
mc <162787712+mc-str@users.noreply.github.com>
2026-04-21 18:31:58 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,4 +2,14 @@ window.appConfig = {
     startDate: "2026-04-18 12:50:00",
     copyright: "© 2026 矩阵方块",
     siteName: "MC指令音乐站"
+    communityButton: {
+        text: "加入交流群",
+        url: "https://qun.qq.com/universal-share/share?ac=1&authKey=lvd76wJzCEJY1LWTMEIpeFxu4Mvd6NSEPFXlCojZIN6%2Fwo1mfE3ZVo1JjLaf4hYr&busi_data=eyJncm91cENvZGUiOiIxOTIxNTQ1MDUiLCJ0b2tlbiI6IjdLVmkvc2RrR0c1UXVUZXpka0RJYlBDZ25HQUxkUzcvQkFXVmRQTjZjdFNlR2xuRWhhbWZvWExwYmMxa1UvMVEiLCJ1aW4iOiIzMTQ4NzA1ODQ2In0%3D&data=dhxHxwZdy-vYaBroYPSZU2zbnfKVux6D9EsyihBNzBfERTVgS1_hdyixq7io2NhC27F2EFRPkWQe53IkoD0tBQ&svctype=4&tempid=h5_group_info",
+        icon: "fab fa-qq"
+    },
+    matrixButton: {
+        text: "矩阵方块软件网",
+        url: "jzfk.indevs.in",
+        icon: "fas fa-cubes"
+    }
 };
